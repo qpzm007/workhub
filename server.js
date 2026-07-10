424b2d4c35abf74df0237389aa0c00df24b9577a
qpzm007 <qpzm007@users.noreply.github.com>
2026-07-10 17:37:14 +0900

    Bump version to v1.2.3 & add updates

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = 45678;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // Request logger middleware
 app.use((req, res, next) => {
